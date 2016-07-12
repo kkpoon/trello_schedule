7ce0fca127a4b062a7ad598705409d4088fa3466
kkpoon <noopkk@gmail.com>
2016-07-12 16:51:24 +0800

    become reactive

--- app.js
+++ app.js
@@ -1,80 +1,76 @@
 //get your token here: https://trello.com/app-key Token link
-var app_key = process.env.TRELLO_APP_KEY || "";
-var token = process.env.THELLO_TOKEN || "";
+const APP_KEY = process.env.TRELLO_APP_KEY || "";
+const TOKEN = process.env.THELLO_TOKEN || "";
 
-var trello = require("./trello.js")(app_key, token);
+import Rx from "@reactivex/rxjs";
+import Trello from "node-trello";
+
 var dateFormat = require('dateformat');
 var nodemailer = require('nodemailer');
 var schedule = require("./schedule.config");
-var template_board_id = '577b357e91f47f23b5e16dbc';
 var email_to = [
   'wah.wong@abc.com'
 ]
 
+const t = new Trello(APP_KEY, TOKEN);
+
+function cloneBoard(name, callback) {
+  return t.post("/1/boards", {
+    name: name,
+    idOrganization: "opsteam12"
+    idBoardSource: "577b357e91f47f23b5e16dbc",
+    prefs_permissionLevel: "org"
+  }, callback);
+}
+
+function getBoard(id, callback) {
+  return t.get(`/1/boards/${id}`, {
+    lists: "open",
+    list_fields: "name",
+    fields: "name,desc,url"
+  }, callback);
+}
+
+function createCard(name, desc, due, idList, callback) {
+  return t.post("/1/cards", { name, desc, due, idList }, callback);
+}
+
 function main() {
   const d = new Date();
   const w = d.getDay();
-  schedule.days
-    .filter((day) => +day.dayofweek === w)
-    .forEach(day => {
-      createBoardOfTheDate(dateFormat(d, "yyyymmdd"), function(err, create_board_response){
-        if (err) {
-          console.error(err.message);
-          return;
-        }
-        const board_id = create_board_response.id;
-        const board_name = create_board_response.name;
-        console.log("[" + d + "] Created board " + board_name + " with ID: " + board_id);
-        trello.getBoard(board_id).then(function(get_board_response) {
-          lists = get_board_response.lists;
-          //console.log(lists);
-          board_url = get_board_response.url;
-          lists
-            .filter(({name}) => name === 'Todo')
-            .forEach(function({id}) {
-              let cardno = 0;
-              const promises = day.report.map(function(task) {
-                return new Promise(function(resolve, reject) {
-                  const options = {
-                    "name": task.account,
-                    "desc": task.schedule + "\r\n" + task.type + "\r\nAM: " + task.am + "\r\nRemarks: " + task.remarks + "\r\nLead Time: " + task.time,
-                    "due":dateFormat(d, "yyyy-mm-dd") + " 6:00",
-                    "idList":id
-                  }
-                  trello.createCard(options)
-                    .then(function(body) {
-                      cardno++;
-                      return resolve(body);
-                    })
-                    .catch(function(err) {
-                      return reject(err);
-                    });
-                });
-              });
-              Promise.all(promises).then(function(values) {
-                console.log("Finish adding");
-                sendEmail(email_to, 'Report Board created for ' + dateFormat(d, "yyyy-mm-dd"), cardno + " cards created to Todo list\r\n<br/>"+board_url);
-              });
-            });
-          });
-        });
-      });
-}
+  const yymmdd = dateFormat(d, "yyyymmdd");
 
-function createBoardOfTheDate(date, callback) {
-  var options = {
-    "name": 'report_'+date,
-    "idOrganization":"opsteam12",
-    "idBoardSource":template_board_id,
-    "prefs_permissionLevel":"org"
-  }
-  trello.clone(options)
-    .then(function(json) {
-      callback(null, json);
+  const boards$ = Rx.Observable
+    .of(...schedule.days)
+    .filter(day => +day.dayofweek === w)
+    .map(day => Rx.Observable.bindCallback(cloneBoard(`report_${yymmdd}`)))
+    .mergeAll()
+    .map(board => {
+      const { name, id } = board;
+      console.log(`[${d}] Created board ${name} with ID: ${id}`);
+      return Rx.Observable.bindCallback(getBoard(id));
     })
-    .catch(function(err) {
-      callback(err);
-    });
+    .mergeAll();
+
+  const todoLists$ = boards$
+    .map(({ url, lists }) => board.lists.map(list => Object.assign({}, list, {board_url: url})))
+    .filter(list => list.name === "Todo");
+
+  const cards$ = todoLists$
+    .map(list => Rx.Observable
+      .of(...day.report)
+      .map(task => Rx.Observable.fromPromise(trello.createCard({
+        "name": task.account,
+        "desc": `${task.schedule}\r\n${task.type}\r\nAM: ${task.am}\r\nRemarks: ${task.remarks}\r\nLead Time: ${task.time}`,
+        "due": dateFormat(d, "yyyy-mm-dd") + " 6:00",
+        "idList": list.id
+      })))
+      .mergeAll();
+    )
+    .mergeAll();
+
+  console.log("Finish adding");
+  sendEmail(email_to, 'Report Board created for ' + dateFormat(d, "yyyy-mm-dd"), cardno + " cards created to Todo list\r\n<br/>"+board_url);
 }
 
 function sendEmail(recipents, subject, body) {
